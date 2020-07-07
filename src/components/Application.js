deed53aa099813287aa91605de8ca335b1937711
Eric Ho <endonoho@gmail.com>
2020-07-07 15:44:26 -0700

    Add schedule component to app

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 
 import "components/Application.scss";
 import DayList from 'components/DayList';
+import Appointment from "components/Appointment";
 
 const days = [
     {
@@ -21,10 +22,64 @@ const days = [
     },
 ];
 
+const appointments = [
+    {
+        id: 1,
+        time: "12pm",
+    },
+    {
+        id: 2,
+        time: "1pm",
+        interview: {
+            student: "Lydia Miller-Jones",
+            interviewer: {
+                id: 1,
+                name: "Sylvia Palmer",
+                avatar: "https://i.imgur.com/LpaY82x.png",
+            }
+        }
+    },
+    {
+        id: 3,
+        time: "2pm",
+        interview: {
+            student: "David Lee Jones",
+            interviewer: {
+                id: 2,
+                name: "Robby Dango",
+                avatar: "https://i.imgur.com/LpaY82x.png",
+            }
+        }
+    },
+    {
+        id: 4,
+        time: "3pm",
+    },
+    {
+        id: 5,
+        time: "4pm",
+        interview: {
+            student: "Jacky Chance",
+            interviewer: {
+                id: 3,
+                name: "Jason Borne",
+                avatar: "https://i.imgur.com/LpaY82x.png",
+            }
+        }
+    }
+];
+
 export default function Application(props) {
   const [ day, setDay ] = useState("Monday");
 
-  console.log(day);
+  const appointment = appointments.map(appointment => {
+      return (
+          <Appointment
+            key={appointment.id}
+            {...appointment}
+          />
+      )
+  })
 
   return (
     <main className="layout">
@@ -49,7 +104,9 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointment}
+
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
