a5a7c03f1c87abd2d5bb28480be8e67a1623edb6
Eric Ho <endonoho@gmail.com>
2020-07-11 21:16:30 -0700

    Refactor code

---
+++ src/reducers/application.js
@@ -0,0 +1,21 @@
+const SET_DAY = "SET_DAY";
+const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+const SET_INTERVIEW = "SET_INTERVIEW"
+
+export { SET_DAY, SET_APPLICATION_DATA, SET_INTERVIEW }
+
+export default function reducer(state, action) {
+    switch (action.type) {
+        case SET_DAY:
+            return { ...state, ...action.payload }
+        case SET_APPLICATION_DATA:
+            return { ...state, ...action.payload }
+        case SET_INTERVIEW: {
+            return { ...state, ...action.appointment }
+        }
+        default:
+            throw new Error(
+                `Tried to reduce with unsupported action type: ${action.type}`
+            );
+    }
+}
